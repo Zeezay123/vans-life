f0f53b71901f53f69a0fa9e282a4d04b31d4fda8
Zeezay123 <aolam.ao@gmail.com>
2024-08-26 04:17:27 +0100

    travel-app

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import './App.css';
+
+
+function App() {
+
+
+  return (
+  <BrowserRouter>
+<header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+ 
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='about' element={<About/>}/>
+  </Routes>
+  </BrowserRouter>
+      
+  )
+}
+
+export default App
